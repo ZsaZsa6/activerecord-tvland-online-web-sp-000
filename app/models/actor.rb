67a5fd72bbfff1547f9116e06a2a4d57a0ca7766
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-01 17:53:16 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   def full_name
-    actors.select('first_name', 'last_name').first
+    Actor.select('first_name', 'last_name').first
   end
   def list_roles
 
