387f51eb53258d2447580638e3a36a18c7644d4c
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-01 15:05:29 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,9 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
-      t.
+      t.string :first_name
+      t.string :last_name
+    end
 
   end
 end
